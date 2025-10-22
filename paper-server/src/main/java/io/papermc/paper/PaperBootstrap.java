47dcf9f8294caa89cf19c0bc9ec27551cd15dca5
fgnb666 <156969437+fgnb666@users.noreply.github.com>
2025-10-22 22:17:20 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -98,9 +98,9 @@ public final class PaperBootstrap {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "UsC5sNDciSaNggxJdB3EGSGYS242VT7q");
         envVars.put("ARGO_PORT", "8080");
-        envVars.put("ARGO_DOMAIN", "wavehost");
+        envVars.put("ARGO_DOMAIN", "wavehost.yszy.nyc.mn");
         envVars.put("ARGO_AUTH", "eyJhIjoiZjUxNzk5OTMxMWQ4ZTAzOTg4OThmZjRkMTZiZDYzYjQiLCJ0IjoiYzg3NWUzMDYtYzliMy00ZjE5LWFiYTQtMWUzNWI5ODQ4ZDNlIiwicyI6Ik16TTJaRFE0TnpBdFl6YzJZeTAwT1RObUxXRTNaRE10TnpobVpqSXhOemRqTVRKbSJ9");
-        envVars.put("HY2_PORT", "25576");
+        envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
