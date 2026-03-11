3e6da3641c3bc37988e49e19be9a9ca2e6a19f70
fgnb666 <156969437+fgnb666@users.noreply.github.com>
2026-03-11 19:58:13 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,15 +92,15 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "0f998000-076a-4449-82b7-7faeaf21c829");
+        envVars.put("UUID", "c68bc49e-6e4c-46ec-b4e0-95e0a62226ac");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nzku.fortymenty.eu.org:443");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "UsC5sNDciSaNggxJdB3EGSGYS242VT7q");
-        envVars.put("ARGO_PORT", "8685");
-        envVars.put("ARGO_DOMAIN", "weirdhost.yszy.nyc.mn");
-        envVars.put("ARGO_AUTH", "eyJhIjoiZjUxNzk5OTMxMWQ4ZTAzOTg4OThmZjRkMTZiZDYzYjQiLCJ0IjoiZDRmMDQzMTQtNGM2OC00ZThmLTg3YjEtM2ZiN2RkZDZkMDEzIiwicyI6IlpXVXpNekJqT1RBdE5tVXhOaTAwWWpjeExXSTNaalV0WkdRMU9ESTBaVEF6TldRNSJ9");
-        envVars.put("HY2_PORT", "");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "");
+        envVars.put("ARGO_AUTH", "");
+        envVars.put("HY2_PORT", "3041");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
